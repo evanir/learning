ef816020bc9a74ca8fcf1474a99ea195beb47abf
evanir_jr <evanir@evol.inf.br>
2022-07-16 14:39:44 -0300

    Limpeza

--- ionicApiApp/src/app/shared/shared.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-  exports: [ CommonModule, ReactiveFormsModule, IonicModule ]
-})
-export class SharedModule { }
--- udemy/home_desk/IonicFirestoreTasks/src/app/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { ReactiveFormsModule } from '@angular/forms';
-
-@NgModule({
-  exports: [CommonModule, ReactiveFormsModule, IonicModule]
-})
-export class SharedModule {}
